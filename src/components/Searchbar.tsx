8ff68fd930ca721efa70e9c2f9d20376601a5e13
Waizul <waizul@gmail.com>
2024-05-25 20:53:41 +0600

    nav searchbar and nav icons added.

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,11 +1,20 @@
-'use client'
+"use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const Searchbar = () => {
-  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  const router = useRouter();
 
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get("name") as string;
+
+    if (name) {
+      router.push(`/list?name=${name}`);
+    }
   };
 
   return (
@@ -14,6 +23,7 @@ const Searchbar = () => {
       onSubmit={handleSearch}
     >
       <input
+        name="name"
         placeholder="Search"
         className="flex-1 bg-transparent outline-none"
       />
