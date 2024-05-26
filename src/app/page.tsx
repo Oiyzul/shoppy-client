1f339ad8b95d7bae9984cbbfb738541ce8d38181
Waizul <waizul@gmail.com>
2024-05-26 12:34:20 +0600

    Category list page design.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import CategoryList from "@/components/CategoryList";
 import Slider from "@/components/Slider";
 
 const HomePage = () => {
   return (
     <div className="">
       <Slider />
+      <div className="mt-24">
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">Categories</h1>
+        <CategoryList />
+      </div>
     </div>
   );
 };
